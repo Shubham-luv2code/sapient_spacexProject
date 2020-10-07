33393053540d4cec34598bec77cc5d8f2ffd020f
Shubham Arora <shubhamarora@Shubhams-MacBook-Air.local>
2020-10-07 17:44:34 +0530

    Updates

---
+++ precache-manifest.9a39e5b827ac3e3afe7f4835476b8da9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea296b10a32edda55ef317c1a1d86ddf",
+    "url": "/sapient_spacexProject/index.html"
+  },
+  {
+    "revision": "9bd620ba083b636aaed4",
+    "url": "/sapient_spacexProject/static/css/main.a2d5932e.chunk.css"
+  },
+  {
+    "revision": "68cec25cf2a313fd8446",
+    "url": "/sapient_spacexProject/static/js/2.f471c62c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sapient_spacexProject/static/js/2.f471c62c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd620ba083b636aaed4",
+    "url": "/sapient_spacexProject/static/js/main.c0be200e.chunk.js"
+  },
+  {
+    "revision": "7bab4bf13350502bf887",
+    "url": "/sapient_spacexProject/static/js/runtime-main.b31bca2a.js"
+  },
+  {
+    "revision": "c369cf4f77c6909d162630aae59041e6",
+    "url": "/sapient_spacexProject/static/media/rocket.c369cf4f.jpg"
+  }
+]);
